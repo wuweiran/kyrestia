d56e1ff13edef1e97124f8fbde9e10e99ca77bca
wuweiran <wwrustc@mail.ustc.edu.cn>
2022-01-15 17:18:47 +0800

    BPMN: Implement basic function of exclusive gateway and parallel gateway

--- core/src/main/java/clan/midnight/kyrestia/bpmn/model/gateway/ExclusiveGateway.java
+++ core/src/main/java/clan/midnight/kyrestia/bpmn/model/gateway/ExclusiveGateway.java
@@ -1,6 +1,39 @@
 package clan.midnight.kyrestia.bpmn.model.gateway;
 
 import clan.midnight.kyrestia.bpmn.model.IdBasedElement;
+import clan.midnight.kyrestia.bpmn.model.flow.SequenceFlow;
+import clan.midnight.kyrestia.bpmn.model.support.XmlReference;
+import clan.midnight.kyrestia.infra.spi.TypeBinding;
+import clan.midnight.kyrestia.model.Node;
+import clan.midnight.kyrestia.model.RuntimeExecutionPoint;
 
-public class ExclusiveGateway extends IdBasedElement {
+import java.util.ArrayList;
+
+@TypeBinding("bpmn:exclusiveGateway")
+public class ExclusiveGateway extends IdBasedElement implements Node {
+    @XmlReference(type = XmlReference.Type.CHILD_ELEMENT_REF, value = "bpmn:incoming")
+    private final ArrayList<SequenceFlow> incomingSequenceFlowList = new ArrayList<>(2);
+
+    @XmlReference(type = XmlReference.Type.CHILD_ELEMENT_REF, value = "bpmn:outgoing")
+    private final ArrayList<SequenceFlow> outGoingSequenceFlowList = new ArrayList<>(1);
+
+    @Override
+    public void enter(RuntimeExecutionPoint executionPoint) {
+        // do nothing
+    }
+
+    @Override
+    public void execute(RuntimeExecutionPoint executionPoint) {
+        // do nothing
+    }
+
+    @Override
+    public Node leave(RuntimeExecutionPoint executionPoint) {
+        for (SequenceFlow sequenceFlow : outGoingSequenceFlowList) {
+            if (sequenceFlow.match(executionPoint)) {
+                return sequenceFlow.getTargetNode();
+            }
+        }
+        return null;
+    }
 }
--- core/src/main/java/clan/midnight/kyrestia/bpmn/model/gateway/ParallelGateway.java
+++ core/src/main/java/clan/midnight/kyrestia/bpmn/model/gateway/ParallelGateway.java
@@ -1,6 +1,38 @@
 package clan.midnight.kyrestia.bpmn.model.gateway;
 
 import clan.midnight.kyrestia.bpmn.model.IdBasedElement;
+import clan.midnight.kyrestia.bpmn.model.flow.SequenceFlow;
+import clan.midnight.kyrestia.bpmn.model.support.XmlReference;
+import clan.midnight.kyrestia.infra.spi.TypeBinding;
+import clan.midnight.kyrestia.model.Node;
+import clan.midnight.kyrestia.model.RuntimeExecutionPoint;
 
-public class ParallelGateway extends IdBasedElement {
+import java.util.ArrayList;
+
+@TypeBinding("bpmn:parallelGateway")
+public class ParallelGateway extends IdBasedElement implements Node {
+    @XmlReference(type = XmlReference.Type.CHILD_ELEMENT_REF, value = "bpmn:incoming")
+    private final ArrayList<SequenceFlow> incomingSequenceFlowList = new ArrayList<>(2);
+
+    @XmlReference(type = XmlReference.Type.CHILD_ELEMENT_REF, value = "bpmn:outgoing")
+    private final ArrayList<SequenceFlow> outGoingSequenceFlowList = new ArrayList<>(1);
+
+    @Override
+    public void enter(RuntimeExecutionPoint executionPoint) {
+        // do nothing
+    }
+
+    @Override
+    public void execute(RuntimeExecutionPoint executionPoint) {
+        for (SequenceFlow sequenceFlow : outGoingSequenceFlowList) {
+            if (sequenceFlow.match(executionPoint)) {
+                executionPoint.newExecutionPointOn(sequenceFlow.getTargetNode());
+            }
+        }
+    }
+
+    @Override
+    public Node leave(RuntimeExecutionPoint executionPoint) {
+        return null;
+    }
 }
